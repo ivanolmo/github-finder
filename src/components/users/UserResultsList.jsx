f2a3c3d3bc7432489c5c025d0f26a9488c1e463b
Ivan Olmo <ivan.olmo@gmail.com>
2022-01-08 19:43:09 -0600

    Add contextApi. Wrapped app in context provider.

--- src/components/users/UserResultsList.jsx
+++ src/components/users/UserResultsList.jsx
@@ -1,23 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useContext } from 'react';
+
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResultsList() {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await response.json();
-
-    setUsers(data);
-    setIsLoading(false);
-  };
+  const { users, isLoading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
